b00065f1907fb5c6a1b9af6590a18171da206f34
Pedro-Ol1veira <113263052+Pedro-Ol1veira@users.noreply.github.com>
2024-06-22 15:14:41 -0300

    Update config.js

--- miniblog/src/firebase/config.js
+++ miniblog/src/firebase/config.js
@@ -1,18 +1,2 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBnWWrivGl8iymPj6NDSw9AyzfdpY1uAB0",
-    authDomain: "miniblig-ea399.firebaseapp.com",
-    projectId: "miniblig-ea399",
-    storageBucket: "miniblig-ea399.appspot.com",
-    messagingSenderId: "105424375750",
-    appId: "1:105424375750:web:74088b1756575baf75ecc7"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-export { db };
 
